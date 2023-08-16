01ba987a425b9912d9e1bc04eb6375cea081a0ee
VilinKehayov <vilinkexayov@gmail.com>
2023-08-16 16:23:37 +0300

    Made the defaultToken: MainView

--- modern-app/app/desktop/src/Application.js
+++ modern-app/app/desktop/src/Application.js
@@ -1,28 +1,30 @@
-Ext.define('ModernApp.Application', {
-	extend: 'Ext.app.Application',
-	name: 'ModernApp',
-	requires: ['ModernApp.*'],
-	defaultToken: 'homeview',
+Ext.define("ModernApp.Application", {
+  extend: "Ext.app.Application",
+  name: "ModernApp",
+  requires: ["ModernApp.*"],
+  defaultToken: "MainView",
 
-	removeSplash: function () {
-		Ext.getBody().removeCls('launching')
-		var elem = document.getElementById("splash")
-		elem.parentNode.removeChild(elem)
-	},
+  removeSplash: function () {
+    Ext.getBody().removeCls("launching");
+    var elem = document.getElementById("splash");
+    elem.parentNode.removeChild(elem);
+  },
 
-	launch: function () {
-		this.removeSplash()
-		var whichView = 'app-main'
-		Ext.Viewport.add([{xtype: whichView}])
-	},
+  launch: function () {
+    this.removeSplash();
+    var whichView = "app-main";
+    Ext.Viewport.add([{ xtype: whichView }]);
+  },
 
-	onAppUpdate: function () {
-		Ext.Msg.confirm('Application Update', 'This application has an update, reload?',
-			function (choice) {
-				if (choice === 'yes') {
-					window.location.reload();
-				}
-			}
-		);
-	}
+  onAppUpdate: function () {
+    Ext.Msg.confirm(
+      "Application Update",
+      "This application has an update, reload?",
+      function (choice) {
+        if (choice === "yes") {
+          window.location.reload();
+        }
+      }
+    );
+  },
 });
