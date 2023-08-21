eb3db5db10d154f37ad816d7b8cf4157c27947d8
VilinKehayov <vilinkexayov@gmail.com>
2023-08-21 09:03:58 +0300

    changed cancel button to close the dialog

--- modern-app/app/desktop/src/view/main/UserForm.js
+++ modern-app/app/desktop/src/view/main/UserForm.js
@@ -1,47 +1,47 @@
-Ext.define('ModernApp.view.main.UserForm', {
-    extend: 'Ext.form.Panel',
-    xtype: 'userform',
-    
-    title: 'Fill up the form',
-    floating: true,
-    centered: true,
-    modal: true,
-    
-    width: 400,
-    
-    items: [
-        {
-            xtype: 'textfield',
-            name: 'name',
-            label: 'Name'
-        },
-        {
-            xtype: 'textfield',
-            name: 'username',
-            label: 'Username'
-        },
-        {
-            xtype: 'textfield',
-            name: 'email',
-            label: 'Email'
-        },
-        {
-            xtype: 'textfield',
-            name: 'address',
-            label: 'Address'
-        }
-    ],
-    
-    buttons: [
-        {
-            text: 'Submit',
-            handler: 'onSubmitUserClick'
-        },
-        {
-            text: 'Cancel',
-            handler: function (btn) {
-                btn.up('userform').destroy();
-            }
-        }
-    ]
+Ext.define("ModernApp.view.main.UserForm", {
+  extend: "Ext.form.Panel",
+  xtype: "userform",
+
+  title: "Fill up the form",
+  floating: true,
+  centered: true,
+  modal: true,
+
+  width: 400,
+
+  items: [
+    {
+      xtype: "textfield",
+      name: "name",
+      label: "Name",
+    },
+    {
+      xtype: "textfield",
+      name: "username",
+      label: "Username",
+    },
+    {
+      xtype: "textfield",
+      name: "email",
+      label: "Email",
+    },
+    {
+      xtype: "textfield",
+      name: "address",
+      label: "Address",
+    },
+  ],
+
+  buttons: [
+    {
+      text: "Submit",
+      handler: "onSubmitUserClick",
+    },
+    {
+      text: "Cancel",
+      handler: function (btn) {
+        btn.up("userdialog").destroy(); // "userform close?"
+      },
+    },
+  ],
 });
