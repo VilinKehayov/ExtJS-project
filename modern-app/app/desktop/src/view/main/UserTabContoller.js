867a78589cf2f9d85970473644e07d56dcc7288d
VilinKehayov <vilinkexayov@gmail.com>
2023-08-22 09:11:57 +0300

    All handlers are triggered and working

--- modern-app/app/desktop/src/view/main/UserTabContoller.js
+++ modern-app/app/desktop/src/view/main/UserTabContoller.js
@@ -5,7 +5,7 @@ Ext.define("ModernApp.view.main.UsersTabController", {
   requires: [
     "ModernApp.view.main.UserForm", // Add this line to include UserForm
     "ModernApp.view.main.UserDialog",
-    "ModernApp.view.main.DeleteUserDialog"
+    "ModernApp.view.main.DeleteUserDialog",
   ],
 
   onAddUserClick: function () {
@@ -28,48 +28,13 @@ Ext.define("ModernApp.view.main.UsersTabController", {
 
   onConfirmDelete: function () {
     console.log("User deleted");
-    // var me = this,
-    //   usersTab = me.getView(),
-    //   deleteDialog = Ext.create("ModernApp.view.main.DeleteUserDialog", {
-    //     // Optional: Pass any necessary data to the dialog
-    //   });
-
-    // usersTab.add(deleteDialog);
-    // deleteDialog.show();
   },
 
   onSubmitUserClick: function () {
     console.log("Created new user");
-    // var me = this,
-    //   form = me.lookupReference("userform");
-
-    // if (form.isValid()) {
-    //   var values = form.getValues(),
-    //     usersStore = me.getViewModel().getStore("users");
-
-    //   usersStore.add(values);
-
-    //   // Save data to server (POST request)
-    //   usersStore.sync({
-    //     success: function () {
-    //       Ext.toast("User added successfully", "Success");
-    //       form.destroy();
-    //     },
-    //     failure: function () {
-    //       Ext.toast("Failed to add user", "Error");
-    //     },
-    //   });
-    // }
   },
 
-  onBeforeShow: function(dialog) {
-
+  onBeforeShow: function (dialog) {
     console.log("Prefilled the form");
-    // var userForm = dialog.down('userform'); // Get the reference to the userform
-    // var selectedRecord = this.getSelectedRecord(); // Implement this method to get the selected record
-    
-    // if (userForm && selectedRecord) {
-    //   userForm.setData(selectedRecord.getData()); // Pre-fill the form with the selected record's data
-    // }
   },
 });
