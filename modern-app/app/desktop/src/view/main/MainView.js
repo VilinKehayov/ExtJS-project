9cff2f6ce285ff7944184885ff08544dcce8d051
VilinKehayov <vilinkexayov@gmail.com>
2023-08-25 15:47:00 +0300

    Cleared unused code and change names camilCase

--- modern-app/app/desktop/src/view/main/MainView.js
+++ modern-app/app/desktop/src/view/main/MainView.js
@@ -1,35 +1,15 @@
-// Ext.define('ModernApp.view.main.MainView', {
-//     extend: 'Ext.Container',
-//     xtype: 'mainview',
-//     controller: 'mainviewcontroller',
-//     viewModel: {
-//         type: 'mainviewmodel'
-//     },
-//     requires: [
-//     'Ext.layout.Fit'
-//     ],
-//     layout: 'fit',
-//     items: [
-//         { xtype: 'navview',    reference: 'navview',    docked: 'left',   bind: {width:  '{navview_width}'}, listeners: { select: "onMenuViewSelectionChange"} },
-//         { xtype: 'headerview', reference: 'headerview', docked: 'top',    bind: {height: '{headerview_height}'} },
-//         { xtype: 'footerview', reference: 'footerview', docked: 'bottom', bind: {height: '{footerview_height}'} },
-//         { xtype: 'centerview', reference: 'centerview' },
-//         { xtype: 'detailview', reference: 'detailview', docked: 'right',  bind: {width:  '{detailview_width}'}  },
-//     ]
-// });
-
 Ext.define("ModernApp.view.main.MainView", {
-  extend: "Ext.tab.Panel",
-  xtype: "app-main",
+    extend: "Ext.tab.Panel",
+    xtype: "app-main",
 
-  requires: ["ModernApp.view.main.UsersTab", "ModernApp.view.main.PhotosTab"],
+    requires: ["ModernApp.view.main.UsersTab", "ModernApp.view.main.PhotosTab"],
 
-  items: [
-    {
-      xtype: "userstab",
-    },
-    {
-      xtype: "photostab",
-    },
-  ],
+    items: [
+        {
+            xtype: "usersTab",
+        },
+        {
+            xtype: "photosTab",
+        },
+    ],
 });
