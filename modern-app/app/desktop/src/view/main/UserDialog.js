d1262d4c06a5cde1991b57d6ba7eebea101bb40e
VilinKehayov <vilinkexayov@gmail.com>
2023-08-25 15:48:18 +0300

    New controller, viewmodel, title bind,

--- modern-app/app/desktop/src/view/main/UserDialog.js
+++ modern-app/app/desktop/src/view/main/UserDialog.js
@@ -1,20 +1,43 @@
 Ext.define("ModernApp.view.main.UserDialog", {
-  extend: "Ext.Dialog",
-  xtype: "userdialog",
+    extend: "Ext.Dialog",
+    xtype: "userDialog",
 
-  controller: "userstab",
-  viewModel: "userstab",
+    requires: [
+        "ModernApp.view.main.UserDialogController",
+        "ModernApp.view.main.UserDialogViewModel",
+    ],
 
-  title: "",
-  closable: true,
-  defaultFocus: "userform button[text=Submit]",
-  bodyPadding: 250,
-  modal: true,
-  maxWidth: 550,
-
-  items: [
-    {
-      xtype: "userform",
+    controller: "userdialogcontroller", // Use the created controller
+    viewModel: {
+        type: "userdialogviewmodel", // Use the created ViewModel
     },
-  ],
+
+    bind: {
+        title: "{dialogTitle}", // Bind the title to the ViewModel variable
+    },
+
+    closable: true,
+    defaultFocus: "userForm button[text=Submit]",
+    bodyPadding: 250,
+    modal: true,
+    maxWidth: 550,
+
+    items: [
+        {
+            xtype: "userForm",
+        },
+    ],
+
+    buttons: [
+        {
+            text: "Submit",
+            handler: "onSubmitUserClick",
+        },
+        {
+            text: "Cancel",
+            handler: function (btn) {
+                btn.up("userDialog").destroy(); // "userform close?"
+            },
+        },
+    ],
 });
