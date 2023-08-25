4e81d2e2f71f55ed0d4333721f605035077738e1
VilinKehayov <vilinkexayov@gmail.com>
2023-08-25 15:51:17 +0300

    Moved the buttons, use defaults, no VM and Contr.

--- modern-app/app/desktop/src/view/main/UserForm.js
+++ modern-app/app/desktop/src/view/main/UserForm.js
@@ -1,62 +1,34 @@
 Ext.define("ModernApp.view.main.UserForm", {
-  extend: "Ext.form.Panel",
-  xtype: "userform",
+    extend: "Ext.form.Panel",
+    xtype: "userForm",
 
-  controller: "userstab",
-  viewModel: "userstab",
+    title: "Fill up the form",
+    floating: true,
+    centered: true,
+    modal: true,
+    width: 400,
 
-  title: "Fill up the form",
-  floating: true,
-  centered: true,
-  modal: true,
+    // Use 'defaults' to set common properties for child items
+    defaults: {
+        xtype: "textfield",
+    },
 
-  width: 400,
-
-  items: [
-    {
-      xtype: "textfield",
-      name: "name",
-      label: "Name",
-      bind: {
-        value: "{record.name}", // Bind to the name property of the ViewModel's record
-      },
-    },
-    {
-      xtype: "textfield",
-      name: "username",
-      label: "Username",
-      bind: {
-        value: "{record.username}", // Bind to the name property of the ViewModel's record
-      },
-    },
-    {
-      xtype: "textfield",
-      name: "email",
-      label: "Email",
-      bind: {
-        value: "{record.email}", // Bind to the name property of the ViewModel's record
-      },
-    },
-    {
-      xtype: "textfield",
-      name: "address",
-      label: "Address",
-      bind: {
-        value: "{record.address.city}", // Bind to the name property of the ViewModel's record
-      },
-    },
-  ],
-
-  buttons: [
-    {
-      text: "Submit",
-      handler: "onSubmitUserClick",
-    },
-    {
-      text: "Cancel",
-      handler: function (btn) {
-        btn.up("userform").destroy(); // "userform close?"
-      },
-    },
-  ],
+    items: [
+        {
+            name: "name",
+            label: "Name",
+        },
+        {
+            name: "username",
+            label: "Username",
+        },
+        {
+            name: "email",
+            label: "Email",
+        },
+        {
+            name: "address",
+            label: "Address",
+        },
+    ],
 });
