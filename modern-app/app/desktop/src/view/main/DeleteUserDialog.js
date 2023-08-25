b803a8d3ae33a5adb5df217f719cb1132b847e7a
VilinKehayov <vilinkexayov@gmail.com>
2023-08-25 15:45:04 +0300

    Separate controller, viewmodel, store refresh

--- modern-app/app/desktop/src/view/main/DeleteUserDialog.js
+++ modern-app/app/desktop/src/view/main/DeleteUserDialog.js
@@ -1,43 +1,45 @@
 Ext.define("ModernApp.view.main.DeleteUserDialog", {
-  extend: "Ext.Dialog",
-  xtype: "deleteuserdialog",
+    extend: "Ext.Dialog",
+    xtype: "deleteUserDialog",
 
-  controller: "userstab",
-  viewModel: "userstab",
+    requires: ["ModernApp.view.main.DeleteUserDialogController"],
 
-  title: "Delete User",
-  closable: true,
-  defaultFocus: "button[text=Yes]",
-  bodyPadding: 30,
-  modal: true,
-  maxWidth: 100,
-  html: "Are you sure you want to delete this user?",
+    controller: "deleteuserdialogcontroller",
+    viewModel: { data: { userId: null } },
 
-  buttons: [
-    {
-      text: "Yes",
-      handler: function (btn) {
-        const deleteUserDialog = btn.up("deleteuserdialog"),
-          record = deleteUserDialog.getRecord(); // Get the passed record
+    title: "Delete User",
+    closable: true,
+    defaultFocus: "button[text=Yes]",
+    bodyPadding: 30,
+    modal: true,
+    maxWidth: 100,
+    html: "Are you sure you want to delete this user?",
 
-        record.erase({
-          success: function () {
-            console.log("The User was destroyed!");
-            const usersTab = record.getUsersTab();
-            usersTab.fireEvent("onRecordDelete", record);
-            deleteUserDialog.destroy();
-          },
-          failure: function () {
-            console.error("Failed to destroy the User");
-          },
-        });
-      },
-    },
-    {
-      text: "No",
-      handler: function (btn) {
-        btn.up("deleteuserdialog").destroy();
-      },
-    },
-  ],
+    buttons: [
+        {
+            text: "Yes",
+            handler: function (btn) {
+                const deleteUserDialog = btn.up("deleteUserDialog"),
+                    record = deleteUserDialog.getRecord(); // Get the passed record
+
+                record.erase({
+                    success: function () {
+                        console.log("The User was destroyed!");
+                        // Trigger the custom event to refresh the store
+                        ModernApp.app.fireEvent("refreshusersstore");
+                        deleteUserDialog.destroy();
+                    },
+                    failure: function () {
+                        console.error("Failed to destroy the User");
+                    },
+                });
+            },
+        },
+        {
+            text: "No",
+            handler: function (btn) {
+                btn.up("deleteUserDialog").destroy();
+            },
+        },
+    ],
 });
