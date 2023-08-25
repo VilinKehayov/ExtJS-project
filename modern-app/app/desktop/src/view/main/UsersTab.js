b10989a0ad8583e6d9cd5e13183b633b6c066136
VilinKehayov <vilinkexayov@gmail.com>
2023-08-25 15:52:10 +0300

    new names of the VM and CONT.,

--- modern-app/app/desktop/src/view/main/UsersTab.js
+++ modern-app/app/desktop/src/view/main/UsersTab.js
@@ -2,90 +2,97 @@ Ext.require("Ext.layout.Fit");
 Ext.require("Ext.grid.cell.Widget");
 
 Ext.define("ModernApp.view.main.UsersTab", {
-  extend: "Ext.Panel",
-  xtype: "userstab",
-  title: "Users",
+    extend: "Ext.Panel",
+    xtype: "usersTab",
+    title: "Users",
 
-  layout: { type: "fit" },
+    layout: { type: "fit" },
 
-  requires: [
-    "ModernApp.store.UserStore",
-    "ModernApp.view.main.UsersTabViewModel",
-    "ModernApp.view.main.UsersTabController",
-  ],
+    requires: [
+        "ModernApp.store.UserStore",
+        "ModernApp.view.main.UsersTabViewModel",
+        "ModernApp.view.main.UsersTabController",
+    ],
 
-  controller: "userstab",
-  viewModel: "userstab",
+    controller: "usersTabController",
+    viewModel: "usersTabViewModel",
 
-  items: [
-    {
-      xtype: "grid",
-      bind: {
-        store: "{users}",
-      },
-      columns: [
-        { text: "ID", dataIndex: "id", flex: 1 },
-        { text: "Name", dataIndex: "name", flex: 1 },
-        { text: "Username", dataIndex: "username", flex: 1 },
-        { text: "Email", dataIndex: "email", flex: 1 },
+    // tools: [
+    //     {
+    //         type: "plus",
+    //         handler: "onAddUserClick", // Attach the handler for the Add User button
+    //     },
+    // ],
+
+    items: [
         {
-          text: "Address",
-          tpl: "{address.city}",
-          xtype: "templatecolumn",
-          flex: 1,
-        },
-        {
-          text: "Actions",
-          flex: 1,
-          cell: {
-            xtype: "widgetcell",
-            viewModel: true,
-            align: "center",
-            widget: {
-              xtype: "button",
-              iconCls: "x-fa fa-home", // Icon for the menu button
-              menu: [
-                {
-                  text: "Edit",
-                  iconCls: "x-fa fa-edit",
-                  handler: "onEditUserClick",
-                },
-                {
-                  text: "Delete",
-                  iconCls: "x-fa fa-trash",
-                  handler: "onDeleteUserClick",
-                },
-              ],
+            xtype: "grid",
+            bind: {
+                store: "{users}",
             },
-          },
-        },
-        {
-          text: "Add User ",
-          flex: 1,
-          cell: {
-            xtype: "widgetcell",
-            viewModel: true,
-            align: "center",
-            widget: {
-              xtype: "button",
-              iconCls: "x-fa fa-plus",
-              width: "100%",
-              handler: "onAddUserClick",
-              items: [
+            columns: [
+                { text: "ID", dataIndex: "id", flex: 1 },
+                { text: "Name", dataIndex: "name", flex: 1 },
+                { text: "Username", dataIndex: "username", flex: 1 },
+                { text: "Email", dataIndex: "email", flex: 1 },
                 {
-                  xtype: "button",
-                  text: "+",
+                    text: "Address",
+                    xtype: "templatecolumn",
+                    tpl: "{address.city}",
+                    flex: 1,
                 },
-              ],
+                {
+                    text: "Actions",
+                    flex: 1,
+                    cell: {
+                        xtype: "widgetcell",
+                        viewModel: true,
+                        align: "center",
+                        widget: {
+                            xtype: "button",
+                            iconCls: "x-fa fa-home", // Icon for the menu button
+                            menu: [
+                                {
+                                    text: "Edit",
+                                    iconCls: "x-fa fa-edit",
+                                    handler: "onEditUserClick",
+                                },
+                                {
+                                    text: "Delete",
+                                    iconCls: "x-fa fa-trash",
+                                    handler: "onDeleteUserClick",
+                                },
+                            ],
+                        },
+                    },
+                },
+                {
+                    text: "Add User ",
+                    flex: 1,
+                    cell: {
+                        xtype: "widgetcell",
+                        viewModel: true,
+                        align: "center",
+                        widget: {
+                            xtype: "button",
+                            iconCls: "x-fa fa-plus",
+                            width: "100%",
+                            handler: "onAddUserClick",
+                            items: [
+                                {
+                                    xtype: "button",
+                                    text: "+",
+                                },
+                            ],
+                        },
+                    },
+                },
+                // Add other columns as needed
+            ],
+            selectable: {
+                columns: true, // Can select cells and rows, but not columns
+                extensible: true, // Uses the draggable selection extender
             },
-          },
         },
-        // Add other columns as needed
-      ],
-      selectable: {
-        columns: true, // Can select cells and rows, but not columns
-        extensible: true, // Uses the draggable selection extender
-      },
-    },
-  ],
+    ],
 });
