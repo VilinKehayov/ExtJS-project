e720a3dc1842b0338bada699904a91cef2b855e0
VilinKehayov <vilinkexayov@gmail.com>
2023-08-25 15:46:28 +0300

    Controllerdelete dialog, listener store refresh

---
+++ modern-app/app/desktop/src/view/main/deleteUserDialogController.js
@@ -0,0 +1,22 @@
+Ext.define("ModernApp.view.main.DeleteUserDialogController", {
+    extend: "Ext.app.ViewController",
+    alias: "controller.deleteuserdialogcontroller",
+
+    listen: {
+        global: {
+            refreshusersstore: "refreshUsersStore",
+        },
+    },
+
+    refreshUsersStore: function () {
+        const grid = this.getView().down("grid"),
+            store = grid.getStore();
+
+        if (store) {
+            console.log("Refreshing the User store..");
+            store.load(); // Reload the store to reflect changes
+        } else {
+            console.error("Record store not found.");
+        }
+    },
+});
