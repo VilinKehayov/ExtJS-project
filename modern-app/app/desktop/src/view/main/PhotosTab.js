7551a64f92f500f73601f88b7a8791d87cd7a686
VilinKehayov <vilinkexayov@gmail.com>
2023-08-25 15:47:20 +0300

    xtype with camilCase

--- modern-app/app/desktop/src/view/main/PhotosTab.js
+++ modern-app/app/desktop/src/view/main/PhotosTab.js
@@ -1,31 +1,31 @@
 Ext.define("ModernApp.view.main.PhotosTab", {
-  extend: "Ext.Panel",
-  xtype: "photostab",
-  title: "Photos",
+    extend: "Ext.Panel",
+    xtype: "photosTab",
+    title: "Photos",
 
-  items: [
-    {
-      xtype: "list",
-      scrollable: true,
-      height: "720px",
-      store: {
-        type: "json",
-        proxy: {
-          type: "rest",
-          url: "https://jsonplaceholder.typicode.com/photos",
-          reader: {
-            type: "json",
-            rootProperty: "",
-          },
+    items: [
+        {
+            xtype: "list",
+            scrollable: true,
+            height: "720px",
+            store: {
+                type: "json",
+                proxy: {
+                    type: "rest",
+                    url: "https://jsonplaceholder.typicode.com/photos",
+                    reader: {
+                        type: "json",
+                        rootProperty: "",
+                    },
+                },
+                autoLoad: true,
+            },
+            itemTpl: [
+                '<div style="text-align: center;">',
+                '<img src="{thumbnailUrl}" alt="Photo" style="max-width: 100%;">',
+                "<div>{title}</div>",
+                "</div>",
+            ],
         },
-        autoLoad: true,
-      },
-      itemTpl: [
-        '<div style="text-align: center;">',
-        '<img src="{thumbnailUrl}" alt="Photo" style="max-width: 100%;">',
-        "<div>{title}</div>",
-        "</div>",
-      ],
-    },
-  ],
+    ],
 });
