8b837a454c9c88babb7a328d25aacfa575bb1f2c
VilinKehayov <vilinkexayov@gmail.com>
2023-09-07 11:12:27 +0300

    pageSize,start,limit,page Params, totalProperty

--- modern-app/app/desktop/src/store/UserStore.js
+++ modern-app/app/desktop/src/store/UserStore.js
@@ -5,12 +5,17 @@ Ext.define('ModernApp.store.UserStore', {
     extend: 'Ext.data.Store',
     alias: 'store.userstore',
     model: 'ModernApp.model.UserModel',
+    pageSize: 2,
     proxy: {
         type: 'rest',
-        url: 'http://localhost:3000/users', //https://jsonplaceholder.typicode.com/users
+        url: 'http://localhost:3000/users',
+        startParam: '_start',
+        limitParam: '_limit',
+        pageParam: '_page',
         reader: {
             type: 'json',
             rootProperty: 'items',
+            totalProperty: 'totalCount',
         },
     },
     autoLoad: true,
