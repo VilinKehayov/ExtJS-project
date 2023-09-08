0b425c87cc4a03645b95eabc84ff9b9824004ab1
VilinKehayov <vilinkexayov@gmail.com>
2023-09-08 14:12:35 +0300

    Moved the Yes handler inside controller.

--- modern-app/app/desktop/src/view/main/DeleteUserDialog.js
+++ modern-app/app/desktop/src/view/main/DeleteUserDialog.js
@@ -24,28 +24,7 @@ Ext.define('ModernApp.view.main.DeleteUserDialog', {
     buttons: [
         {
             text: 'Yes',
-            handler: function (btn) {
-                const deleteUserDialog = btn.up('deleteUserDialog'),
-                    // recordId = deleteUserDialog.config.recordId; // Access the recordId directly from the config
-                    recordId = deleteUserDialog.getViewModel().get('recordId');
-
-                // Create a new instance of the User model with the recordId
-                const user = Ext.create('ModernApp.model.UserModel', {
-                    id: recordId,
-                });
-
-                user.erase({
-                    success: function () {
-                        console.log('The User was deleted!');
-                        // Trigger the custom event to refresh the store
-                        ModernApp.app.fireEvent('refreshusersstore');
-                        deleteUserDialog.destroy();
-                    },
-                    failure: function () {
-                        console.error('Failed to delete the User');
-                    },
-                });
-            },
+            handler: 'YesButtonClick',
         },
         {
             text: 'No',
